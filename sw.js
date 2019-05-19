4f79aea9ddb5b466839109f8a7143a7d85afbbbd
Phil Housley <undeconstructed@gmail.com>
2019-05-19 18:00:54 +0100

    Trying to do service worker

---
+++ sw.js
@@ -0,0 +1,26 @@
+// https://github.com/mdn/pwa-examples/blob/master/a2hs/sw.js
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('1').then(function(cache) {
+     return cache.addAll([
+       '',
+       'index.html',
+       'icon.png',
+       'css/normalize.css',
+       'css/main.css',
+       'js/main.js',
+       'js/util.js'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  // console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
