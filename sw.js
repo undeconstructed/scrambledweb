fe3fd50174a6cf5df307ade61690e8c89e36c8ad
Phil Housley <undeconstructed@gmail.com>
2019-10-22 11:36:08 +0100

    Is this how sw.js is supposed to update?

--- sw.js
+++ sw.js
@@ -1,26 +1,44 @@
-// https://github.com/mdn/pwa-examples/blob/master/a2hs/sw.js
+const VERSION = '0.3r2'
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('1').then(function(cache) {
-     return cache.addAll([
-       'index.html',
-       'index.js',
-       'icon.png',
-       'css/normalize.css',
-       'css/main.css',
-       'js/classis.js',
-       'js/main.js',
-       'js/util.js'
-     ]);
-   })
- );
-});
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('install', function (e) {
+  // console.log('sw install', VERSION)
+  e.waitUntil(
+    caches.open(VERSION).then(function (cache) {
+      return cache.addAll([
+        'index.html',
+        'index.js',
+        'icon.png',
+        'css/normalize.css',
+        'css/main.css',
+        'js/classis.js',
+        'js/main.js',
+        'js/util.js',
+      ])
     })
-  );
-});
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  // console.log('sw activate', VERSION)
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      let ps = []
+      for (let key of keys) {
+        if (key !== VERSION) {
+          // console.log('sw deleting cache', key)
+          ps.push(caches.delete(key))
+        }
+      }
+      return Promise.all(ps)
+    })
+  )
+})
+
+self.addEventListener('fetch', function (e) {
+  // console.log('sw fetch', VERSION, e.request)
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request)
+    })
+  )
+})
